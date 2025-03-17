5502b669874bdfc9209b22b061dfa8ade4fbeb67
Anton Ryabenko <djedra@yandex.ru>
2025-03-18 01:47:09 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -3,22 +3,17 @@ import Team from './team.js';
 import canIterate from './canIterate.js';
 
 const team1 = new Team();
-const character1 = new Character('Bobr', 'animal', 100, 1, 15, 5);
-const character2 = new Character('Kurva', 'animal', 100, 1, 15, 5);
+const character1 = new Character('Лучник', 'Bowman', 50, 1, 40, 10);
+const character2 = new Character('Маг', 'Mage', 50, 1, 30, 5);
 team1.addCharacter(character1);
 team1.addCharacter(character2);
 
-// Исп генератор
-const teamCharacters = team1[Symbol.iterator]();
-console.log(teamCharacters.next().value);
-console.log(teamCharacters.next().value);
+// Итерация
+for (const character of team1) {
+  console.log(character);
+}
 
-// Исп итератор
-/*const teamIterator = team1.iterator;
-console.log(teamIterator.next().value);
-console.log(teamIterator.next().value);*/
-
-// Проверяем canIterate
+// Проверка canIterate
 console.log(canIterate(new Map())); // true
 console.log(canIterate(new Set())); // true
 console.log(canIterate(null)); // false
